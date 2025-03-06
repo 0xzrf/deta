0d5e36b0ae65847212f7aeef4d0185f18d4f9973
0xzrf <krishh16@yahoo.com>
2025-03-06 19:31:55 +0530

    added new stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,23 @@
 module.exports = {
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
   rules: {
     '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }]
-  }
-} 
+  },
+}; 
