1c29db469217734311f03849b1577f106cc1fab2
harshana15 <harshana555prajapati@gmail.com>
2025-03-07 06:54:07 +0530

    coin gecko fix

--- types/coingecko.d.ts
+++ types/coingecko.d.ts
@@ -1,10 +1,9 @@
-import React from 'react';
-
-declare namespace _JSX {
-  interface _IntrinsicElements {
+/* eslint-disable no-undef */
+declare namespace JSX {
+  interface IntrinsicElements {
     'coingecko-coin-price-chart-widget': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
       'coin-id': string;
-      currency: string;
+      'currency': string;
       height: string;
       locale: string;
       'background-color': string;
